0dd9171334fd38bb4895513cff8dfccefb73a2b3
wangrunqian <793341127@qq.com>
2020-01-08 11:29:47 +0800

    mysql的url增加时区参数

--- sluggard-core/src/main/java/com/mylq/sluggard/core/common/enums/DbTypeEnum.java
+++ sluggard-core/src/main/java/com/mylq/sluggard/core/common/enums/DbTypeEnum.java
@@ -28,7 +28,7 @@ public enum DbTypeEnum implements BaseEnum<Integer> {
     /**
      * MySQL
      */
-    MYSQL(0, "mysql", "com.mysql.cj.jdbc.Driver", "jdbc:mysql://{0}:{1}/{2}"),
+    MYSQL(0, "mysql", "com.mysql.cj.jdbc.Driver", "jdbc:mysql://{0}:{1}/{2}?serverTimezone=GMT%2B8"),
     /**
      * Oracle
      */
